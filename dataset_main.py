f030ecabf426452fab381d146e63890d3567169c
anilyerrapragada <anilkumar2710@hotmail.com>
2021-03-21 00:00:25 -0500

    rearranged code to enable use of for-loops in jupyter

--- dataset_main.py
+++ dataset_main.py
@@ -10,7 +10,7 @@ max_SNR = 50
 SNR_dB = range(0, max_SNR, 5)
 fading = 1 # 0 - AWGN only, 1 - with fading channel
 num_ant = 2 # Number of antennas
-bit_codebook = 2 # Bits per codebook index
+bit_codebook = 1 # Bits per codebook index
 num_classes = 2**bit_codebook # Number of classes that the classifer needs to learn
 
 pls_params = PLSParameters(num_ant, bit_codebook)
